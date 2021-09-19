1e5951bda8fe26ba46aea35c2b0f5b2e468082a4
Hunter Page <PAGEHM1@ETSU.EDU>
2021-09-18 22:16:53 -0400

    New Stuff

--- Team_InstructionDecoder/Program.cs
+++ Team_InstructionDecoder/Program.cs
@@ -6,7 +6,17 @@ namespace Team_InstructionDecoder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the binary stream: ");
+
+            char instructionPartOne = (char)Console.Read();
+            char instructionPartTwo = (char)Console.Read();
+            char instructionSpace = (char)Console.Read();
+
+        }
+
+        public void readMemory()
+        {
+
         }
     }
 }
