663b6546c46d9743c113b9e051e33eac4aab0127
Hunter Page <PAGEHM1@ETSU.EDU>
2021-09-19 19:36:49 -0400

    Added Processor Class to create a concept of what it could look like

---
+++ Team_Instruction_Fetch_Decode_Execute/Processor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Instruction_Fetch_Decode_Execute
+{
+    class Processor
+    {
+        public string[] Memory { get; set; }
+
+        public ushort Accumulator { get; set; }
+
+        public ushort X_Register { get; set; }
+
+        public ushort ProgramCounter { get; set; }
+
+        public ushort StackRegister { get; set; }
+
+        public bool NegativeFlag { get; set; }
+
+        public bool CarryFlag { get; set; }
+
+        public bool ZeroFlag { get; set; }
+
+        public bool TrueFlag { get; set; }
+    }
+}
---
+++ Team_Instruction_Fetch_Decode_Execute/Register.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Instruction_Fetch_Decode_Execute
+{
+    class Register
+    {
+        public string Name { get; set; }
+
+        public byte value { get; set; }
+
+
+    }
+}
